6f1d2fb6238a07b1103bc0fc385b6ded941e2075
Sam C. Finan <samcfinan@gmail.com>
2020-10-29 18:17:23 -0400

    Find & input quantity working

--- index.ts
+++ index.ts
@@ -2,15 +2,80 @@ import { readFileSync } from 'fs'
 import * as yaml from 'js-yaml'
 import { chromium } from 'playwright'
 
-const config = yaml.safeLoad(readFileSync('./purchases.yaml', 'utf-8'))
+interface Config {
+  special_offer_url?: string
+  purchases: WineToPurchase[]
+  payment: {
+    card_num: number
+    card_cvv: number
+    card_exp: string
+  }
+}
+
+interface WineToPurchase {
+  id: number
+  quantity: number
+}
+
+const config = yaml.safeLoad(readFileSync('./purchases.yaml', 'utf-8')) as Config
+
+const baseURL = 'https://www.vintagesshoponline.com/vintages'
 
 async function main() {
+  // Let's put these in a hashmap for performance and normalize the data
+  const purchaseMap: { [keyof: string]: { id: string; quantity: string } } = {}
+  config.purchases.map((wine) => {
+    const paddedWineID = wine.id.toString().padStart(7, '0')
+
+    purchaseMap[paddedWineID] = {
+      id: paddedWineID,
+      quantity: wine.quantity.toString(),
+    }
+  })
+
+  console.log(purchaseMap)
+
   const browser = await chromium.launch({ headless: false })
 
+  // Go to Classics Collection
   const page = await browser.newPage()
-  await page.goto('https://www.vintagesshoponline.com/vintages/ClassicsCollection.aspx')
-  await page.click('text=October')
-  document.querySelectorAll('')
+  await page.goto(`${baseURL}/ClassicsCollection.aspx`, { waitUntil: 'networkidle' })
+
+  // Find latest collection, navigate to page
+  const latestCollection = await page.$('text=October')
+  const link = await latestCollection?.getAttribute('href')
+  await page.goto(`${baseURL}/${link}`!, { waitUntil: 'networkidle' })
+
+  const tableRows = await page.$$('tbody > tr')
+
+  for (const row of tableRows) {
+    const rowData = await row.$$('td')
+    // Skip table rows
+    if (rowData.length < 4) {
+      continue
+    }
+
+    const idNode = await rowData[1].$('span')
+    const id = await idNode?.innerText()!
+
+    // Grab purchase data for wine
+    const purchaseData = purchaseMap[id]
+
+    // Skip wines not in purchase config file
+    if (!purchaseData) {
+      continue
+    }
+
+    const quantityInput = await rowData[7].$('input')
+    await quantityInput?.fill(purchaseData.quantity)
+  }
+
+  // for await (const row of tableRows) {
+  //   const cells = await row.$$eval('td', (item) => {})
+  //   cells.map(async (c) => {
+  //     console.log(await c.innerHTML())
+  //   })
+  // }
 }
 
 main()
